8e95b4adcc616c04e2bc9ee0dabf0fd0d1df195c
trepixert <pixert@mail.ru>
2019-04-22 12:03:23 +0400

    ,,,

--- InternetShop/InternetShopServiceDAL/BindingModels/ComponentProductBindingModel.cs
+++ InternetShop/InternetShopServiceDAL/BindingModels/ComponentProductBindingModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InternetShopServiceDAL.ViewModel {
-    public class ComponentViewModel {
+namespace InternetShopServiceDAL.BindingModels {
+    public class ComponentProductBindingModel {
     }
 }
