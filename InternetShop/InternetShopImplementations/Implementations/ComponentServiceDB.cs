9d7f02424f0ffac1d94ab1190604b85dddde7447
trepixert <pixert@mail.ru>
2019-05-29 13:18:33 +0400

    ??

---
+++ InternetShop/InternetShopImplementations/Implementations/ComponentServiceDB.cs
@@ -0,0 +1,32 @@
+﻿using InternetShopServiceDAL.BindingModels;
+using InternetShopServiceDAL.Interfaces;
+using InternetShopServiceDAL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShopImplementations.Implementations {
+    public class ComponentServiceDB : IComponentService {
+        public void AddComponent(ComponentBindingModel model) {
+            throw new NotImplementedException();
+        }
+
+        public void DelComponent(int id) {
+            throw new NotImplementedException();
+        }
+
+        public ComponentViewModel GetElement(int id) {
+            throw new NotImplementedException();
+        }
+
+        public List<ComponentViewModel> GetList() {
+            throw new NotImplementedException();
+        }
+
+        public void UpdComponent(ComponentBindingModel model) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InternetShop/InternetShopServiceDAL/Interfaces/IMainAdmin.cs
@@ -0,0 +1,21 @@
+﻿using InternetShopServiceDAL.BindingModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetShopServiceDAL.Interfaces {
+    public interface IMainAdmin {
+        List<ComponentBindingModel> GetComponents();
+        ComponentBindingModel GetComponent(int id);
+        List<ProductBindingModel> GetProducts();
+        ProductBindingModel GetProduct(int id);
+        void CreateProduct(List<ComponentBindingModel> Components);
+        void UpdateProduct(List<ComponentBindingModel> Components);
+        void DeleteProduct(List<ComponentBindingModel> Components);
+        void CreateComponent(ComponentBindingModel component);
+        void UpdateComponent(ComponentBindingModel component);
+        void DeleteComponent(ComponentBindingModel component);
+    }
+}
